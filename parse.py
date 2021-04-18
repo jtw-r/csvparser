e3e99bd2d0dd5192b17f5fe118ed317655186148
jtw-r <jonathan.reiterman@gmail.com>
2021-04-18 11:18:36 -0700

    Updated

--- parse.py
+++ parse.py
@@ -10,20 +10,47 @@ import csv
 #
 # ARGUMENTS     '*' are REQUIRED
 #   *csv_file   | type: STRING  | CASE SENSITIVE | The .csv file you wish to reference
-#   *csv_column | type: STRING  | CASE SENSITIVE | The column name you wish to pull data from
-#                                                   within the .csv file
 #    csv_sep    | type: CHAR    | CASE SENSITIVE | The delimiter that the specific .csv file
 #                                                   uses. Will reference the get_delimiter()
 #                                                   function if no value is specified
 #
 # RETURNS a list of the fields within
 #
-def parse_columns(csv_file="file.csv", csv_column="", csv_sep=""):
+def parse(csv_file="file.csv", csv_sep=""):
+    """
+
+    :param csv_file:
+    :param csv_sep:
+    :return:
+    """
     if csv_sep == "":
         df = pd.read_csv(csv_file, sep=get_delimiter(csv_file))
     else:
         df = pd.read_csv(csv_file, sep=csv_sep)
-    return df[csv_column].values
+    return df
+
+
+def get_column(csv_struct: list, csv_column: str):
+    return csv_struct[csv_column].values
+
+
+def get_columns(csv_struct: list, csv_columns: list):
+    aggregate = []
+    for _index in csv_columns:
+        _data = csv_struct[_index]
+        aggregate.append(_data)
+
+    return aggregate
+
+
+#
+# Remove duplicate fields
+#
+def condense(csv_struct: list):
+    _zipped = {}
+    if len(csv_struct) == 2:
+        _zipped = dict(zip(csv_struct[0], csv_struct[1]))
+    return _zipped
 
 
 #
@@ -42,7 +69,6 @@ def parse_columns(csv_file="file.csv", csv_column="", csv_sep=""):
 def get_delimiter(csv_file="file.csv"):
     sniffer = csv.Sniffer()
     sample_bytes = 32
-    dialect = sniffer.sniff(open("file.csv").read(sample_bytes))
+    dialect = sniffer.sniff(open(csv_file).read(sample_bytes))
 
     return dialect.delimiter
-
--- wrapper.py
+++ wrapper.py
@@ -3,5 +3,17 @@ import parse as p
 
 # Call the parse() function and specify 1) the file location/name, 2) the column name you wish to
 #   pull data from. It will return a list that you can print out or iterate through.
-data = p.parse_columns("file.csv", "Company")
+data = p.parse("file.csv")
 print(data)
+print("---")
+
+data_column = p.get_column(data, "Company")
+print(data_column)
+print("---")
+
+data_multi_columns = p.get_columns(data, ["Company", "Email"])
+print(data_multi_columns)
+print("---")
+
+data_condensed = p.condense(data_multi_columns)
+print(data_condensed)
